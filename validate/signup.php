472dc03c5834495648cd2ad2df2f922776161e06
benrobo <alumonabenaiah71@gmail.com>
2021-06-18 15:21:32 +0100

    added javascript logic, php logic

--- validate/signup.php
+++ validate/signup.php
@@ -4,13 +4,13 @@ require("../db/dbh.php");
 
 if(isset($_POST['lname']) && isset($_POST['fname']) && isset($_POST['email']) && isset($_POST['pnumber']) && isset($_POST['pwd']) &&  isset($_POST['name']) && isset($_POST['size']) && isset($_POST['type'])){
     
-    $lname = mysqli_reql_escape_string($conn, $_POST['type'])
-    $fname = mysqli_reql_escape_string($conn, $_POST['type'])
-    $email = mysqli_reql_escape_string($conn, $_POST['type'])
-    $pnumber = mysqli_reql_escape_string($conn, $_POST['type'])
-    $pwd = mysqli_reql_escape_string($conn, $_POST['type'])
-    $name = mysqli_reql_escape_string($conn, $_POST['type'])
-    $size = mysqli_reql_escape_string($conn, $_POST['type'])
+    $lname = mysqli_reql_escape_string($conn, $_POST['type']);
+    $fname = mysqli_reql_escape_string($conn, $_POST['type']);
+    $email = mysqli_reql_escape_string($conn, $_POST['type']);
+    $pnumber = mysqli_reql_escape_string($conn, $_POST['type']);
+    $pwd = mysqli_reql_escape_string($conn, $_POST['type']);
+    $name = mysqli_reql_escape_string($conn, $_POST['type']);
+    $size = mysqli_reql_escape_string($conn, $_POST['type']);
     $type = mysqli_reql_escape_string($conn, $_POST['type']);
 
 }  
