b4107bf0f12593caf919952eb2bd2a5e61d3605d
benrobo <alumonabenaiah71@gmail.com>
2021-06-22 12:11:56 +0100

    nothing to say

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -11,3 +11,29 @@ function  activNewevents(){
     }
 }
 activNewevents()
+
+function HideandSeeEditCont(){
+    let editbtn = document.querySelector(".edit-btn")
+    let editprofilecont = document.querySelector(".edit-profile")
+    let cancelbtn = document.querySelector(".cancel-edit-btn");
+
+    cancelbtn.onclick = (e)=>{
+        e.preventDefault()
+        editprofilecont.classList.remove("show")
+        editprofilecont.classList.add("hide")
+    }
+    editbtn.onclick = ()=>{
+        editprofilecont.classList.remove("hide")
+        editprofilecont.classList.add("show")
+    }
+}
+HideandSeeEditCont()
+
+function logout(){
+    let logoutbtn = document.querySelector(".logout-btn");
+
+    logoutbtn.onclick = ()=>{
+        window.location = "../index.html"
+    }
+}
+logout()
